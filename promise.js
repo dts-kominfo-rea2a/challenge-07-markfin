1b2f8743fc50e47912671c0da8bbd15e218c0f84
markfin <marfinzamarudin@gmail.com>
2022-06-12 08:33:02 +0900

    menambahkan data

--- promise.js
+++ promise.js
@@ -1,8 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async(emosi) => {
+    let x = await promiseTheaterIXX();
+    let y = await promiseTheaterVGC();
+    const arrFilm = (await x).concat(await y);
+    return arrFilm.filter((Element) => Element.hasil === emosi).length;
+}
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
